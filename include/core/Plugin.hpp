6719fe06540654c3cdbc10ff3f6c580ef289583f
wang-edward <edawordwang@gmail.com>
2023-02-18 17:05:28 -0500

    add plugin

---
+++ include/core/Plugin.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "al/graphics/al_Graphics.hpp"
+#include "al/io/al_AudioIO.hpp"
+#include "al/io/al_Window.hpp"
+
+namespace lofi {
+
+class Plugin {
+
+public:
+    // Plugin();
+    // Plugin(const Plugin&);
+    // Plugin(Plugin&&);
+    // Plugin& operator=(const Plugin&);
+    // Plugin& operator=(Plugin&&);
+    // virtual ~Plugin() = 0;
+
+    virtual void onProcess(al::AudioIOData &io) = 0;
+    virtual void onProcess(al::Graphics& g) = 0;
+    virtual void onTriggerOn(const al::Keyboard &k) = 0;
+    virtual void onTriggerOff(const al::Keyboard &k) = 0;
+};
+
+} // namespace lofi 
